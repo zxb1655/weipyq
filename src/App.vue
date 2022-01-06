3ea73fe1c55de85c10937fadf1bd7f8035eff7e1
zxb1655 <58070684+zxb1655@users.noreply.github.com>
2022-01-06 10:15:23 +0800

    更加电脑端打开提示

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script>
 export default {
-  onLaunch: function () {},
+  onLaunch: function () {
+    // #ifdef H5
+    if (navigator.userAgent.indexOf("Mobile") === -1) {
+      uni.showToast({
+        title: "请使用手机访问哦！目前样式没有完全兼容PC端！",
+        icon: "none",
+        duration: 3000,
+      });
+    }
+    // #endif
+  },
   onShow: function () {
     console.log("App Show");
   },
