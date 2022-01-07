11bd29f199343020553038982129121191ce2b40
zxb1655 <58070684+zxb1655@users.noreply.github.com>
2022-01-07 08:38:03 +0800

    更新信息

--- project.config.js
+++ project.config.js
@@ -2,10 +2,10 @@ let config = {
   title: "网页标题", // 网页标题
   useCdnUrl: true, //是否使用svn绝对路径
   baiduUrl: "", // 百度统计链接
-  cdnUrl: "//g.meiway.com.cn/project-template-h5/",
-  grayCdnUrl: "//gray.meiway.com.cn/project-template-h5/",
-  testCdnUrl: "//t.meiway.com.cn/project-template-h5/",
-  innerCdnUrl: "//inner.meiway.cc/project-gcbgc/",
+  cdnUrl: "//cdn.jzzz66.cn/weipyq",
+  grayCdnUrl: "//cdn.jzzz66.cn/weipyq",
+  testCdnUrl: "//cdn.jzzz66.cn/weipyq",
+  innerCdnUrl: "//cdn.jzzz66.cn/weipyq",
   forceHttps: false,
 };
 
